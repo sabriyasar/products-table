4e3c61fcd27b7b406c0fb25d67ce3706070f7da4
sabriyasar <info@sabriyasar.com.tr>
2024-11-06 00:06:14 +0300

    fixed mongodb

--- src/App.js
+++ src/App.js
@@ -3,24 +3,10 @@ import ProductList from "./components/ProductList";
 import "./App.css";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const addProduct = (product) => {
-    setProducts([...products, { id: Date.now(), ...product }]);
-  };
-
-  const deleteProduct = (id) => {
-    setProducts(products.filter((product) => product.id !== id));
-  };
-
-  const filteredProducts = products.filter((product) =>
-    product.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <div className="App">
-      <ProductList products={filteredProducts} deleteProduct={deleteProduct} />
+      <ProductList />
     </div>
   );
 }
